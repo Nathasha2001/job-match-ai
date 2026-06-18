acbf815db357a519f2388271a033cf6367609393
Nathasha <142737136+Nathasha2001@users.noreply.github.com>
2026-06-18 18:02:40 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/assignment/jobmatchai/JobMatchAiApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.jobmatchai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMatchAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobMatchAiApplication.class, args);
+    }
+
+}
